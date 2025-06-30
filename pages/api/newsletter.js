4ffa0b7840c9920753c1648df6aaad0a48d9224c
JS0Nai <working@monarkh.net>
2025-06-30 12:01:36 +0400

    added resend integration with johnny@johnny.ae email to newsletter form on homepage

---
+++ pages/api/newsletter.js
@@ -0,0 +1,31 @@
+
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Email is required' });
+    }
+
+    try {
+      await resend.emails.send({
+        from: 'John Li <johnny@johnny.ae>',
+        to: email,
+        subject: 'hello world',
+        html: '<p>it works!</p>',
+      });
+
+      return res.status(200).json({ message: 'Successfully subscribed!' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).json({ error: 'Something went wrong.' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
