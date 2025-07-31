2cf945359df57b270b10e2c94dd4abfe249d80a2
JS0Nai <working@monarkh.net>
2025-07-31 04:39:58 +0400

    index.js 'ai generation' slider attempted fix - new chat note created

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useInView } from "../hooks/useInView";
